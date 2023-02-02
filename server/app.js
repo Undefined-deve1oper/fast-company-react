7069d89d2644de0f516e13bbbb5d0b1f5822c0a6
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-02 22:21:37 +0600

    work on authorization

--- server/app.js
+++ server/app.js
@@ -2,15 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
+const initDatabase = require("./startUp/initDatabase");
+const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api", routes);
+
 const PORT = config.get("port") ?? 8080;
 
 async function start() {
     try {
+        mongoose.connection.once("open", () => {
+            initDatabase();
+        });
         await mongoose.connect(config.get("mongoUri"));
         app.listen(PORT, () => {
             console.log(
@@ -22,3 +29,5 @@ async function start() {
         process.exit(1);
     }
 }
+
+start();
